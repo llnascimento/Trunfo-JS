0ff9df1f6cced62a7c36524478d5572ee0ea160a
llnascimento <lucaslopesnascimento122@gmail.com>
2024-09-17 17:14:33 -0300

    Ajustabdi e puxando imagens

--- js/trunfo.js
+++ js/trunfo.js
@@ -2,8 +2,17 @@ var btn = document.getElementById("btn_battle");
 
 function btn_poke() {
 
-    let pokemonMaquina = {}
-    let pokemonJogador = {}
+    var disputaInicial = Math.floor(Math.random() * 2) + 1;
+
+    if (disputaInicial === 1) {
+        alert("A disputa iniciará com o jogador 1 (você)");
+        alert("Escolha o atributo que iniciará a disputa");
+    }
+
+    else {
+        alert("A disputa iniciará com o jogador 2 (máquina)");
+        alert("A máquina irá escolher o atributo que iniciará a disputa");
+    }
 
     function gerarPokemon(numeroAleatorio) {
         // Retorna a promessa do `fetch`
@@ -15,11 +24,12 @@ function btn_poke() {
                 // Retorna os dados do Pokémon como um objeto
                 return {
                     nome: data.name,
-                    tipos: data.types.map(typeInfo => typeInfo.type.name).join(" / "),
+                    tipo: data.types.map(typeInfo => typeInfo.type.name).join(" / "),
                     vida: data.stats.find(stat => stat.stat.name === "hp").base_stat,
                     forca: data.stats.find(stat => stat.stat.name === "attack").base_stat,
                     defesa: data.stats.find(stat => stat.stat.name === "defense").base_stat,
-                    velocidade: data.stats.find(stat => stat.stat.name === "speed").base_stat
+                    velocidade: data.stats.find(stat => stat.stat.name === "speed").base_stat,
+                    imagem: data.sprites.front_default
 
                 };
             })
@@ -37,18 +47,39 @@ function btn_poke() {
     gerarPokemon(numeroJogador).then((pokemonJogador) => {
         if (pokemonJogador) {
             console.log("Pokémon do Jogador: ", pokemonJogador);
+            //Irá exibir os atributos do pokemon
+            document.getElementById("info_name_jogador").textContent = pokemonJogador.nome
+            document.getElementById("info_tipo_jogador").textContent = pokemonJogador.tipo
+            document.getElementById("info_vida_jogador").textContent = pokemonJogador.vida
+            document.getElementById("info_forca_jogador").textContent = pokemonJogador.forca
+            document.getElementById("info_defesa_jogador").textContent = pokemonJogador.defesa
+            document.getElementById("info_velocidade_jogador").textContent = pokemonJogador.velocidade
+            // Exibe a imagem do Pokémon
+            document.getElementById("imgJogador").src = pokemonJogador.imagem;
         }
     });
 
     gerarPokemon(numeroMaquina).then((pokemonMaquina) => {
         if (pokemonMaquina) {
             console.log("Pokémon da Máquina: ", pokemonMaquina);
-            // acessando os atributos
-            alert("A defesa da máquina eh: " + pokemonMaquina.defesa)
+             //Irá exibir os atributos do pokemon
+             document.getElementById("info_name_maquina").textContent = pokemonMaquina.nome
+             document.getElementById("info_tipo_maquina").textContent = pokemonMaquina.tipo
+             document.getElementById("info_vida_maquina").textContent = pokemonMaquina.vida
+             document.getElementById("info_forca_maquina").textContent = pokemonMaquina.forca
+             document.getElementById("info_defesa_maquina").textContent = pokemonMaquina.defesa
+             document.getElementById("info_velocidade_maquina").textContent = pokemonMaquina.velocidade
+             // Exibe a imagem do Pokémon
+            document.getElementById("imgMaquina").src = pokemonMaquina.imagem;
+
         }
     });
 
-}
 
+    /* 
+
+   
+    */
+}
 // Adiciona o evento ao clicar no botão
 btn.onclick = btn_poke;
